0ca221c21c049c62c2747b1dbd59e14cea8d7998
Maick Souza <maick_a_s@msn.com>
2019-08-22 18:58:46 -0300

    Modulo React-Native dev01

--- App.js
+++ App.js
@@ -1,40 +1,33 @@
-import React, {Fragment} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <View style={styles.body}>
-        <View style={styles.sectionContainer}>
-          <Text style={styles.sectionTitle}>Hello World</Text>
-          <Text style={styles.sectionTitle}>Hello World</Text>
-        </View>
-      </View>
-    </Fragment>
-  );
-};
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
 const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-  },
   sectionContainer: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 32,
-    paddingHorizontal: 24,
   },
   sectionTitle: {
     fontSize: 24,
     textAlign: 'center',
     backgroundColor: '#F5FCFF',
-    // fontWeight: '600',
-    //  color: Colors.black,
   },
 });
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.body}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Hello World</Text>
+          <Text style={styles.sectionTitle}>Hello World</Text>
+          <Text style={styles.sectionTitle}>Hello World</Text>
+        </View>
+      </View>
+    </>
+  );
+};
 
 export default App;
